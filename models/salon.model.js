4942519133f1bcb81e83b8fdad07b33b5a79cf1c
Jorge Garzon <A01707102@tec.mx>
2025-03-22 15:25:03 -0600

    [ADD] Modal de registro de salón

--- models/salon.model.js
+++ models/salon.model.js
@@ -5,8 +5,9 @@ const salones = [];
 module.exports = class Salon {
 
     // Constructor de la clase
-    constructor(mi_id, mi_capacidad, mi_tipo, mi_nota, mi_campus) {
+    constructor(mi_id, mi_numero, mi_capacidad, mi_tipo, mi_nota, mi_campus) {
         this.id = mi_id;
+        this.numero = mi_nuumero
         this.capacidad = mi_capacidad;
         this.tipo = mi_tipo;
         this.nota = mi_nota;
@@ -15,12 +16,12 @@ module.exports = class Salon {
 
     // Método para guardar de manera persistente un nuevo objeto
     save() {
-        return pool.query('INSERT INTO salon(salon_id, capacidad, tipo, nota, campus) VALUES ($1, $2, $3, $4, $5)', [this.id, this.capacidad, this.tipo, this.nota, this.campus]);
+        return pool.query('INSERT INTO salon(salon_id, capacidad, tipo, nota, campus) VALUES ($1, $2, $3, $4, $5)', [this.numero, this.capacidad, this.tipo, this.nota, this.campus]);
     }
 
     // Método para devolver los objetos del almacenamiento persistente
     static fetchAll() {
-        return pool.query('SELECT salon_id, capacidad, tipo, nota, nombre FROM salon s, campus c WHERE s.campus_id = c.campus_id');
+        return pool.query('SELECT * FROM salon s, campus c WHERE s.campus_id = c.campus_id');
     }
 
     // Método para eliminar un objeto del almacenamiento persistente
