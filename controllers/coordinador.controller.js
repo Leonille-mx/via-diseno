33087848e2bc01329b1bafa5137eef067f43d9dc
DongjuMun <dongjumun03@gmail.com>
2025-03-23 14:08:47 -0600

    [UPDATE] sincroniación de materias versión final

--- controllers/coordinador.controller.js
+++ controllers/coordinador.controller.js
@@ -6,25 +6,46 @@ exports.get_dashboard = (req, res, nxt) => {
     res.render('dashboard_coordinador');
 };
 
-exports.get_materias = (req, res, nxt) => {
-    Materia.fetchAll()
-        .then((result) => {
-            res.render('materias_coordinador', {
-                materias: result.rows,
-            });
-        }).catch((error) => {
-            console.log(error);
+exports.get_materias = async (req, res, nxt) => {
+    try {
+        // Trae todos los registros que tiene atributo abierta
+        // como true
+        const materiasDB = await Materia.fetchAll();
+        // Si hay query string, lo guarda en la variable msg
+        const msg = req.query.msg || null;
+        res.render('materias_coordinador', {
+            materias: materiasDB.rows,
+            msg,
         });
+    } catch(error) {
+        console.log(error);
+    }
 };
 
 exports.post_sincronizar_materias = async (req, res, nxt) => {
     try {
-        const courses = await getAllCourses(); 
-        console.log(courses); 
-        res.redirect('/coordinador/materias');
+        // Trae materias de la API
+        const courses = await getAllCourses();
+
+        // Extrae la 'data' de la JSON respuesta
+        const materiasApi = courses.data;
+
+        // Llama la función sincronizarMaterias() para sincronizar los datos de materia
+        const resultado = await Materia.sincronizarMaterias(materiasApi);
+        // Crea una variable para el mensaje de operación
+        const msg = `La operación fue exitosa!<br>
+                    Insertado: ${resultado.inserted}<br>
+                    Actualizado: ${resultado.updated}<br>
+                    Eliminado: ${resultado.deleted}`;
+
+        // Redirige a la siguiente ruta con el mensaje en query string 
+        // con la función para encodificarlo
+        res.redirect(`/coordinador/materias?msg=${encodeURIComponent(msg)}`);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error para traer datos de materias');
+        // Redirige a la siguiente ruta con un mensaje de error en query string 
+        // con la función para encodificarlo
+        res.redirect(`/coordinador/materias?msg=${encodeURIComponent('La operación fue fracasada')}`);
     }
 };
 
