7a86037779ee1af3638a606654425e92b132c537
Armando <armandofu0705@gmail.com>
2025-03-24 10:48:43 -0600

    [UPDATE] sincronizar alumnos con inscrepcion_completada en status false

--- models/alumno.model.js
+++ models/alumno.model.js
@@ -5,7 +5,7 @@ module.exports = class Alumno {
         this.ivd_id = mi_ivd_id;
         this.semestre = mi_semestre;
         this.regular = mi_regular;
-        this.inscripcion_completa = mi_inscripcion_completa;
+        this.inscripcion_completada = mi_inscripcion_completa;
         this.plan_estudio_id = mi_plan_estudio_id;
     }
 
@@ -28,17 +28,18 @@ module.exports = class Alumno {
 
                 if(!studentDB) {
                     await client.query(
-                        'INSERT INTO alumno (ivd_id, semestre, regular, plan_estudio_id) VALUES ($1, $2, $3, $4)',
-                        [sA.ivd_id, sA.semester, sA.regular, sA.plan_id]
+                        'INSERT INTO alumno (ivd_id, semestre, inscripcion_completada, regular, plan_estudio_id) VALUES ($1, $2, $3, $4, $5)',
+                        [sA.ivd_id, sA.semester, false, sA.regular, sA.plan_id]
                     );
                     inserted ++;
                 } else if (
                       Number(studentDB.semestre) !== Number(sA.semester) ||
-                      Boolean(studentDB.regular) !== Boolean(sA.regular)
+                      Boolean(studentDB.regular) !== Boolean(sA.regular) ||
+                      Number(studentDB.plan_estudio_id) !== Number(sA.plan_id)
                 ) {
                     await client.query(
-                        'UPDATE alumno SET semestre = $1, regular = $2, plan_estudio_id = $3 WHERE alumno_id = $4',
-                        [sA.semester, sA.regular, sA.plan_id, sA.ivd_id]
+                        'UPDATE alumno SET semestre = $1, regular = $2, inscripcion_completada = $3, plan_estudio_id = $4 WHERE alumno_id = $5',
+                        [sA.semester, sA.regular, false, sA.plan_id, sA.ivd_id]
                     );
                     updated++;
                 }
