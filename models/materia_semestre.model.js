6f324b35fe577e318a27b2055db7d7fe3ffbb117
Frida Xcaret Vargas Trejo <fridaxcaretvargastrejo@Fridas-MacBook-Air.local>
2025-04-07 10:29:52 -0600

    [FIX]consulta eliminar

--- models/materia_semestre.model.js
+++ models/materia_semestre.model.js
@@ -16,10 +16,30 @@ module.exports = class MateriaSemestre {
                            WHERE m.materia_id = ms.materia_id`);
     }
     static eliminar(idMateria, idSemestre) {
-        return Promise.all([
-            pool.query('DELETE FROM grupo WHERE materia_id = $1', [idMateria]),
-            pool.query('DELETE FROM materia_semestre WHERE materia_id = $1 AND semestre_id = $2', [idMateria, idSemestre])
-        ]);
+        return pool.query('BEGIN') 
+            .then(() => pool.query(
+                `DELETE FROM resultado_inscripcion 
+                 WHERE grupo_id IN (SELECT grupo_id FROM grupo WHERE materia_id = $1)`,
+                [idMateria]
+            ))
+            .then(() => pool.query(
+                `DELETE FROM grupo_bloque_tiempo 
+                 WHERE grupo_id IN (SELECT grupo_id FROM grupo WHERE materia_id = $1)`,
+                [idMateria]
+            ))
+            .then(() => pool.query(
+                'DELETE FROM grupo WHERE materia_id = $1',
+                [idMateria]
+            ))
+            .then(() => pool.query(
+                'DELETE FROM materia_semestre WHERE materia_id = $1 AND semestre_id = $2',
+                [idMateria, idSemestre]
+            ))
+            .then(() => pool.query('COMMIT')) 
+            .catch(err => {
+                return pool.query('ROLLBACK') 
+                    .then(() => { throw err; });
+            });
     }
     
 }
