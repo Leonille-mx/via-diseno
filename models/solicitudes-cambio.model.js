c08489045aea46437a07b5241326f827471239b6
Frida Xcaret Vargas Trejo <fridaxcaretvargastrejo@Fridas-MacBook-Air.local>
2025-04-21 12:54:48 -0600

    [FIX]orderby solicitdes

--- models/solicitudes-cambio.model.js
+++ models/solicitudes-cambio.model.js
@@ -85,7 +85,7 @@ module.exports = class Solicitud {
         JOIN 
             usuario u ON sc.ivd_id = u.ivd_id
               ORDER BY
-                sc.created_at ASC`);
+                sc.created_at DESC`);
     return result.rows;
   }
 
